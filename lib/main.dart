c7b11afbafe5565724cefdbbd6446a2a7ea045f1
Foluwa <moronfoluwaakintola@gmail.com>
2022-01-01 10:57:25 +0100

    I refactored UI

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,6 @@ class MyApp extends StatelessWidget {
       title: 'Airports Nearby',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       initialRoute: '/',
--- lib/screens/ports.dart
+++ lib/screens/ports.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:country_list_pick/country_list_pick.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -35,15 +36,17 @@ class _PortState extends State<Port> {
     final height = MediaQuery.of(context).size.height;
     //double width = MediaQuery.of(context).size.width;
     return Scaffold(
-      backgroundColor: Constants.primaryColor,
+      // backgroundColor: Constants.grey,
+      backgroundColor: Color(0xfff4f4f4),
       appBar: AppBar(
         backgroundColor: Constants.primaryColor,
+        centerTitle: false,
         title: const Text('Ports'),
         actions: [
           CountryListPick(
             appBar: AppBar(
               backgroundColor: Constants.primaryColor,
-              title: const Text('Pick your country'),
+              title: const Text('Select a country'),
             ),
             theme: CountryTheme(
               isShowFlag: true,
@@ -69,51 +72,90 @@ class _PortState extends State<Port> {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: FutureBuilder<List<PortModel>>(
-            future: _countries,
-            builder: (context, snapshot) {
-              if (!snapshot.hasData)
-                return const Center(child: CircularProgressIndicator());
-              return (snapshot.data.isEmpty)
-                  ? Center(
-                      child: Text('Empty',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Constants.white)))
-                  : ListView.builder(
-                      itemCount: snapshot.data.length,
-                      itemBuilder: (context, index) {
-                        return Card(
-                          child: GestureDetector(
-                            onTap: () {
-                              Navigator.of(context).pushNamed('/port-details',
-                                  arguments: snapshot.data[index]);
-                            },
-                            child: ListTile(
-                              leading: Image.asset(
-                                  'icons/flags/png/${snapshot.data[index].country.toLowerCase()}.png',
-                                  package: 'country_icons'),
-                              title: Text(
-                                '${snapshot.data[index].name} ${snapshot.data[index].state}',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: Constants.black,
+      body: Scrollbar(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: FutureBuilder<List<PortModel>>(
+              future: _countries,
+              builder: (context, snapshot) {
+                if (!snapshot.hasData)
+                  return const Center(child: CircularProgressIndicator());
+                return (snapshot.data.isEmpty)
+                    ? Center(
+                        child: Text('Empty',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Constants.white)))
+                    : ListView.builder(
+                        itemCount: snapshot.data.length,
+                        itemBuilder: (context, index) {
+                          return FadeInUp(
+                            child: Card(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: GestureDetector(
+                                  onTap: () {
+                                    Navigator.of(context).pushNamed(
+                                        '/port-details',
+                                        arguments: snapshot.data[index]);
+                                  },
+                                  child: ListTile(
+                                    // leading: Image.asset(
+                                    //     'icons/flags/png/${snapshot.data[index].country.toLowerCase()}.png',
+                                    //     package: 'country_icons'),
+
+                                    leading: snapshot.data[index].iata.length <
+                                            1
+                                        ? ClipOval(
+                                      child: Container(
+                                        height: 40,
+                                        width: 40,
+                                        decoration: BoxDecoration(color: Colors.pink),
+                                        child: Center(
+                                          child: Text(
+                                            'NIL',
+                                            style: TextStyle(color: Constants.black),
+                                          ),
+                                        ),
+                                      ),
+                                    )
+                                        // : Text(snapshot.data[index].iata),
+                                        : ClipOval(
+                                            child: Container(
+                                              height: 40,
+                                              width: 40,
+                                              decoration: BoxDecoration(color: Colors.pink),
+                                              child: Center(
+                                                child: Text(
+                                                  snapshot.data[index].iata,
+                                                  style: TextStyle(color: Constants.white),
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                    title: Text(
+                                      '${snapshot.data[index].name}',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w700,
+                                        color: Constants.black,
+                                      ),
+                                    ),
+                                    subtitle: Text(
+                                        'City: ${snapshot.data[index].city}  State: ${snapshot.data[index].state}',
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            color: Constants.black)),
+                                    trailing: Icon(
+                                        Icons.arrow_forward_ios_rounded,
+                                        size: height * 0.02),
+                                  ),
                                 ),
                               ),
-                              subtitle: Text(
-                                  '${snapshot.data[index].city}/${snapshot.data[index].state}/ ${snapshot.data[index].country}',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: Constants.black)),
-                              trailing:
-                                  Icon(Icons.play_arrow, size: height * 0.02),
                             ),
-                          ),
-                        );
-                      });
-            }),
+                          );
+                        });
+              }),
+        ),
       ),
       //TODO: Add tab to settings page
     );
